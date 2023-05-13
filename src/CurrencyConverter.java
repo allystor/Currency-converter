37a0712f349b820640a3732ba244f1bccc02993c
allystor <alvarojoao17@gmail.com>
2023-05-13 12:43:52 -0300

    feat: initial

---
+++ src/CurrencyConverter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    private static final double USD_TO_EUR = 0.85;
+    private static final double USD_TO_GBP = 0.72;
+    private static final double USD_TO_BRL = 5.63;
+    private static final double USD_TO_CAD = 1.25;
+    private static final double USD_TO_INR = 73.13;
+    private static final double USD_TO_JPY = 109.77;
+    private static final double USD_TO_CNY = 6.46;
+    private static final double USD_TO_AUD = 1.29;
+
+};
